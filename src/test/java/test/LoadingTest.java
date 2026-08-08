8d40dcfc78ea91e3bd694ae603076841c87a9764
SamDJoyce <Samdjoyce@gmail.com>
2026-08-08 16:04:56 -0400

    Testing fixes
    - revising and simplifying LoadingTest to more clearly indicate when
    something has gone wrong

--- src/test/java/test/LoadingTest.java
+++ src/test/java/test/LoadingTest.java
@@ -8,57 +8,81 @@ import units.instances.UnitInstance;
 import units.options.OptionChoice;
 
 public class LoadingTest {
-//	private final static String wargearLoc  = "src/main/resources/json/wargear.json";
-//	private final static String statLineLoc = "src/main/resources/json/statlines.json";
-//	private final static String modelLoc    = "src/main/resources/json/models.json";
-	private final static String codexLoc    = "src/main/resources/json/codex space marines";
-
+	
+	private final static String spaceMarineCodex = "src/main/resources/json/codex space marines";
 	
 	public static void main(String[] args) {
+		LoadingTest test = new LoadingTest();
+		ArmyBuilder army = test.loadData();
+		// Create a unit description from loaded data
+		UnitDescription description = test.createDescription(army);
+		// Create a unit instance where options can be selected
+		UnitInstance instance = test.createInstance(army, description);
+		// Create and select the 'add marine choice'
+		test.createAndAssignAddMarineChoice(army, instance);
+		// Create and select frag grenades for the squad
+		test.createAndAssignAddFragGrenades(army, instance);
+		// Create and select heavy bolter
+		test.createAndAssignHeavyBolter(army, instance);
+	}
+	
+	private ArmyBuilder loadData() {
 		// Load data from files
-		CodexLoader loader = new CodexLoader(codexLoc);
+		CodexLoader loader = new CodexLoader(spaceMarineCodex);
 		ArmyBuilder army = new ArmyBuilder(loader.loadCodex());
 		System.out.println(army.getCodex().getName());
-		
-		// Create a unit description from loaded data
+		return army;
+	}
+	
+	private UnitDescription createDescription(ArmyBuilder army) {
 		UnitDescription u = army.getCodex().getUnit("tactical squad");
 		System.out.println(u);
-		
-		// Create a unit instance where options can be selected
-		UnitInstance unit = UnitFactory.createInstance(u);
-		army.getRoster().addUnit(unit);
+		return u;
+	}
+	
+	private UnitInstance createInstance(ArmyBuilder army, UnitDescription description) {
+		UnitInstance instance = UnitFactory.createInstance(description);
+		army.getRoster().addUnit(instance);
 		System.out.println("Tactical Squad has " + army.getRoster().getUnits().getFirst() + " models");
 		System.out.println(String.format(
 				"Squad Points Cost: %d", 
-				unit.getTotalPoints()) );
-		
-		// Create and select the 'add marine choice'
+				army.getRoster().getCurrentPoints()) );
+		return instance;
+	}
+	
+	private void createAndAssignAddMarineChoice(ArmyBuilder army, UnitInstance instance) {
 		OptionChoice addMarine = army.getCodex().getChoice("select add tactical marine");
 		System.out.println("Select option to add a marine");
-		unit.addSelection(addMarine);
+		instance.addSelection(addMarine);
 		System.out.printf(
 				"Tactical Squad has %d models\n", 
-				unit.getCurrentSize());
+				instance.getCurrentSize());
 		System.out.println(String.format(
 				"Squad Points Cost: %d", 
-				unit.getTotalPoints()) );
+				instance.getTotalPoints()) );
 		System.out.println("\n");
-		
-		// Create and select frag grenades for the squad
+	}
+	
+	private void createAndAssignAddFragGrenades(ArmyBuilder army, UnitInstance instance) {
 		OptionChoice addFrag = army.getCodex().getChoice("select frag grenades for squad");
-		unit.addSelection(addFrag);
-		System.out.println(unit);
+		instance.addSelection(addFrag);
+		System.out.println(instance);
 		System.out.println(String.format(
 				"Squad Points Cost: %d", 
-				unit.getTotalPoints()) );
+				army.getRoster().getCurrentPoints()) );
 		
-		// Create and select heavy bolter
+	}
+	private void createAndAssignHeavyBolter(ArmyBuilder army, UnitInstance instance) {
 		OptionChoice addHB = army.getCodex().getChoice("select heavy bolter");
-		unit.getModels().get(2).addSelection(addHB);
-		System.out.println(unit);
+		instance.getModels().get(2).addSelection(addHB);
+		System.out.println(instance);
 		System.out.println(String.format(
 				"Squad Points Cost: %d", 
-				unit.getTotalPoints()) );
+				army.getRoster().getCurrentPoints()) );
+	}
+	
+	private void displayRoster() {
+		
 	}
 	
 }
