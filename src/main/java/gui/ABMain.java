d50611cd39c09ce52cb908e1c79c317b89fc8003
Sam Joyce <samdjoyce@gmail.com>
2026-08-19 14:06:17 -0400

    Starting to populate GUI with Codex Data
    - reworked UnitSelectionController.populateUnits() to reduce code
    duplication
    - updated ABMain view to use FXML views
    - moved views folder from java/gui to resources/gui
    - created MainView.XML
    - successfully printed all units into a menu of buttons which respond
    correctly

--- src/main/java/gui/ABMain.java
+++ src/main/java/gui/ABMain.java
@@ -1,10 +1,13 @@
 package gui;
 
+import java.io.IOException;
+
 import builder.ArmyBuilder;
-import gui.views.ABMainView;
+import gui.controllers.UnitSelectionController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import loaders.CodexLoader;
 import roster.Codex;
@@ -16,17 +19,28 @@ public class ABMain extends Application {
     	CodexLoader loader = new CodexLoader("src/main/resources/json/codex space marines");
     	Codex codex  = loader.loadCodex();
     	ArmyBuilder armyBuilder = new ArmyBuilder(codex);
-    	FXMLLoader loaderFXML = new FXMLLoader(
-    				getClass().getResource("/gui/MainView.fxml")
-    			);
     	
-        ABMainView root = new ABMainView();
+        try {
+            FXMLLoader xmlLoader = new FXMLLoader(
+                    getClass().getResource("/gui/MainView.fxml")
+            );
 
-        Scene scene = new Scene(root, 1000, 700);
+            BorderPane root = xmlLoader.load();
 
-        stage.setTitle("Fourth Ed Army Builder");
-        stage.setScene(scene);
-        stage.show();
+            UnitSelectionController controller =
+            		xmlLoader.getController();
+
+            controller.setArmyBuilder(armyBuilder);
+
+            Scene scene = new Scene(root, 1000, 700);
+
+            stage.setTitle("Fourth Ed Army Builder");
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/gui/controllers/UnitSelectionController.java
+++ src/main/java/gui/controllers/UnitSelectionController.java
@@ -1,5 +1,9 @@
 package gui.controllers;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 import builder.ArmyBuilder;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -29,7 +33,21 @@ public class UnitSelectionController {
     private VBox nonePanel;
 
     private ArmyBuilder armyBuilder;
+    
+    private Map<UnitRole, VBox> panels;
 
+    @FXML
+    private void initialize() {
+        panels = Map.of(
+                UnitRole.HQ, hqPanel,
+                UnitRole.ELITES, elitesPanel,
+                UnitRole.TROOPS, troopsPanel,
+                UnitRole.FAST_ATTACK, fastAttackPanel,
+                UnitRole.HEAVY_SUPPORT, heavySupportPanel,
+                UnitRole.NONE, nonePanel
+            );
+    }
+    
     public void setArmyBuilder(ArmyBuilder armyBuilder) {
         this.armyBuilder = armyBuilder;
         populateUnits();
@@ -38,15 +56,16 @@ public class UnitSelectionController {
     private void populateUnits() {
 
         Codex codex = armyBuilder.getCodex();
-        
-        populateRole(codex, UnitRole.HQ, hqPanel);
-        populateRole(codex, UnitRole.ELITES, elitesPanel);
-        populateRole(codex, UnitRole.TROOPS, troopsPanel);
-        populateRole(codex, UnitRole.FAST_ATTACK, fastAttackPanel);
-        populateRole(codex, UnitRole.HEAVY_SUPPORT, heavySupportPanel);
-        populateRole(codex, UnitRole.NONE, nonePanel);
-        
-        
+        for (UnitRole role : UnitRole.values()) {
+        	VBox panel = panels.get(role);
+        	if (panel != null) {
+            	populateRole(
+            			codex, 
+            			role, 
+            			panel);
+        	}
+
+        }
     }
 
     private void populateRole(
